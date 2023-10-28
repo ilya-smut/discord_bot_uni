e15b281afb0ece459ad6320405ee419f93aa2691
ilya-smut <ilya.smut.off.g@gmail.com>
2023-10-28 01:59:45 +0100

    Introducing complaints feature

--- bot.py
+++ bot.py
@@ -3,10 +3,12 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import random
+import complaints
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 W_CHANNEL = os.getenv('WELCOME_CHANNEL')
+core_path = os.getenv('CORE_PATH')
 
 intents = discord.Intents.default()
 intents.members = True
@@ -55,5 +57,66 @@ async def nine_nine(ctx):
 
     response = f'{name} is {word}'
     await ctx.send(response)
+
+
+@bot.command(name='cmp', help='Files a complaint to the bot')
+async def add_complaint(ctx, *args):
+    guild = ctx.guild
+    author = ctx.message.author.name
+    message = ' '.join(args)
+    dir_path = core_path + str(guild)
+    file_path = dir_path + "/complaints.txt"
+    if not os.path.isdir(dir_path):
+        os.mkdir(core_path + str(guild))
+
+    db = complaints.ComplaintsDb()
+    await db.load(file_path)
+
+    key = str(author)
+    start_length = len(key)
+    existent_keys = db.get_keys()
+    while key in existent_keys:
+        if len(key) == start_length:
+            key = key + '1'
+        else:
+            extra = int(key[start_length:])
+            extra += 1
+            key = key[:start_length] + str(extra)
+
+    db.add_complaint(key, message)
+    db.save()
+    await ctx.send('Complaint added with key = ' + str(key) + '; Text = ' + message)
+
+
+@bot.command(name='cmp-all', help='Prints all complaints')
+async def get_complaints(ctx):
+    guild = ctx.guild
+    dir_path = core_path + str(guild)
+    file_path = dir_path + "/complaints.txt"
+    if not os.path.isdir(dir_path):
+        os.mkdir(core_path + str(guild))
+
+    db = complaints.ComplaintsDb()
+    await db.load(file_path)
+
+    bot_response = '**Here is the list of complaints**: \n\n' + '\n\n'.join(db.get_all_complaints())
+
+    await ctx.send(bot_response)
+
+
+@bot.command(name='cmp-get', help='Gets a complaint by complaint-id')
+async def get_complaint(ctx, arg1):
+    guild = ctx.guild
+    dir_path = core_path + str(guild)
+    file_path = dir_path + "/complaints.txt"
+    if not os.path.isdir(dir_path):
+        os.mkdir(core_path + str(guild))
+    db = complaints.ComplaintsDb()
+    await db.load(file_path)
+
+    if arg1 in db.get_keys():
+        await ctx.send(db.get_complaint(arg1))
+    else:
+        await ctx.send('No complaint found')
 
 bot.run(TOKEN)
---
+++ complaints.py
@@ -0,0 +1,62 @@
+import jsonpickle
+import os
+
+
+class ComplaintsDb:
+    def __init__(self):
+        self.complaints = {}
+        self.path = None
+
+    async def load(self, path):
+        self.path = path
+        if os.path.isfile(path):
+            with open(path, 'r') as file:
+
+                contents = file.read()
+
+                if contents != '':
+
+                    extracted_dict = jsonpickle.decode(contents)
+                    if type(extracted_dict) == dict:
+
+                        self.complaints = extracted_dict
+        else:
+            self.path = path
+            with open(path, 'a') as file:
+                pass
+
+    def add_complaint(self, key=None, complaint='I am lazy'):
+        self.complaints[str(key)] = str(key) + " : " + complaint
+
+    def save(self, path=None):
+        if path:
+            with open(path, 'w') as file:
+                encoded_dict = jsonpickle.encode(self.complaints)
+                file.write(encoded_dict)
+        else:
+            with open(self.path, 'w') as file:
+                encoded_dict = jsonpickle.encode(self.complaints)
+                file.write(encoded_dict)
+
+    def drop(self):
+        self.complaints = {}
+
+    def get_keys(self):
+        return self.complaints.keys()
+
+    def get_complaint(self, key):
+        if str(key) in self.get_keys():
+            return self.complaints[str(key)]
+
+    def del_complaint(self, key):
+        if str(key) in self.get_keys():
+            del(self.complaints[str(key)])
+
+    def get_all_complaints(self):
+        complaints = []
+        for key in self.get_keys():
+            complaints.append(self.complaints[key])
+
+        return complaints
+
+
